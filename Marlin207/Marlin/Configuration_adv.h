8c534587051899c8b0e9a494cc6976caf91469bb
Andrew Wilde <arealperson@livingalivemedia.com>
2020-12-12 18:20:17 -0800

    Limit PWM signal to 50%
    Update firmware to limit FAN_MAX_PWM to 50%

--- Marlin207/Marlin/Configuration_adv.h
+++ Marlin207/Marlin/Configuration_adv.h
@@ -434,7 +434,7 @@
  * Define one or both of these to override the default 0-255 range.
  */
 //#define FAN_MIN_PWM 50
-//#define FAN_MAX_PWM 128
+#define FAN_MAX_PWM 128
 
 /**
  * FAST PWM FAN Settings
